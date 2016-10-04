ce76ae46daf40891fddf80c6579679eb752a0cda
Jason Garrett <jason@mavenclinic.com>
2016-10-04 15:00:07 -0400

    swift3

--- Tests/NSCalendarUnit+TimepieceTests.swift
+++ Tests/NSCalendarUnit+TimepieceTests.swift
@@ -12,19 +12,19 @@ import XCTest
 class NSCalendarUnitTestCase: XCTestCase {
     func testCalendarUnitIntervals() {
         // some sanity checks
-        XCTAssertLessThan(NSCalendarUnit.Nanosecond.interval, 1, "")
-        XCTAssertEqual(NSCalendarUnit.Second.interval, 1)
-        XCTAssertEqual(NSCalendarUnit.Minute.interval, 60)
-        XCTAssertEqual(NSCalendarUnit.Hour.interval, 3600)
-        XCTAssertEqual(NSCalendarUnit.Day.interval, 24 * 3600)
-        XCTAssertEqual(NSCalendarUnit.WeekOfYear.interval, 7 * 24 * 3600)
-        XCTAssertEqual(NSCalendarUnit.Month.interval, 30*86400, "")
-        XCTAssertEqual(NSCalendarUnit.Quarter.interval, 146097/400/4*86400, "")
-        XCTAssertEqual(NSCalendarUnit.Year.interval, 146097/400*86400, "")
+        XCTAssertLessThan(NSCalendar.Unit.nanosecond.interval, 1, "")
+        XCTAssertEqual(NSCalendar.Unit.second.interval, 1)
+        XCTAssertEqual(NSCalendar.Unit.minute.interval, 60)
+        XCTAssertEqual(NSCalendar.Unit.hour.interval, 3600)
+        XCTAssertEqual(NSCalendar.Unit.day.interval, 24 * 3600)
+        XCTAssertEqual(NSCalendar.Unit.weekOfYear.interval, 7 * 24 * 3600)
+        XCTAssertEqual(NSCalendar.Unit.month.interval, 30*86400, "")
+        XCTAssertEqual(NSCalendar.Unit.quarter.interval, 146097/400/4*86400, "")
+        XCTAssertEqual(NSCalendar.Unit.year.interval, 146097/400*86400, "")
         
         // some unsupported calendar units should return 0 as interval
-        XCTAssert(NSCalendarUnit.Era.interval == 0)
-        XCTAssert(NSCalendarUnit.Calendar.interval == 0)
-        XCTAssert(NSCalendarUnit.TimeZone.interval == 0)
+        XCTAssert(NSCalendar.Unit.era.interval == 0)
+        XCTAssert(NSCalendar.Unit.calendar.interval == 0)
+        XCTAssert(NSCalendar.Unit.timeZone.interval == 0)
     }
 }
